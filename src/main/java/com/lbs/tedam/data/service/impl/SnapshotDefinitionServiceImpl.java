99f8d98994a0ba1e335a518a92a54e94e55ded18
bozanfaruk <bozanfaruk@gmail.com>
2018-12-25 12:46:41 +0300

    Sonar fixes.

--- src/main/java/com/lbs/tedam/data/service/impl/SnapshotDefinitionServiceImpl.java
+++ src/main/java/com/lbs/tedam/data/service/impl/SnapshotDefinitionServiceImpl.java
@@ -17,6 +17,15 @@
 
 package com.lbs.tedam.data.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.w3c.dom.Document;
+
 import com.lbs.tedam.data.dao.SnapshotDefinitionDAO;
 import com.lbs.tedam.data.service.PropertyService;
 import com.lbs.tedam.data.service.SnapshotDefinitionService;
@@ -25,141 +34,155 @@ import com.lbs.tedam.exception.localized.SnapshotFileNotFoundException;
 import com.lbs.tedam.generator.steptype.AbstractFillTestStepGenerator;
 import com.lbs.tedam.generator.steptype.Generator;
 import com.lbs.tedam.generator.steptype.GeneratorFactory;
-import com.lbs.tedam.model.*;
+import com.lbs.tedam.model.Property;
+import com.lbs.tedam.model.Snapshot;
+import com.lbs.tedam.model.SnapshotDefinition;
+import com.lbs.tedam.model.SnapshotValue;
+import com.lbs.tedam.model.TestCase;
+import com.lbs.tedam.model.TestStep;
 import com.lbs.tedam.util.Constants;
 import com.lbs.tedam.util.EnumsV2.TestStepType;
 import com.lbs.tedam.util.HasLogger;
 import com.lbs.tedam.util.TedamDOMUtils;
 import com.lbs.tedam.util.TedamXPathUtils;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-import org.w3c.dom.Document;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
-public class SnapshotDefinitionServiceImpl extends BaseServiceImpl<SnapshotDefinition, Integer> implements SnapshotDefinitionService, HasLogger {
+public class SnapshotDefinitionServiceImpl extends BaseServiceImpl<SnapshotDefinition, Integer>
+		implements SnapshotDefinitionService, HasLogger {
 
-    /**
-     * long serialVersionUID
-     */
-    private static final long serialVersionUID = 1L;
+	/**
+	 * long serialVersionUID
+	 */
+	private static final long serialVersionUID = 1L;
 
-    @Autowired
-    private BeanFactory beanFactory;
+	@Autowired
+	private transient BeanFactory beanFactory;
 
-    @Autowired
-    private PropertyService propertyService;
+	@Autowired
+	private PropertyService propertyService;
 
-    @Autowired
-    public void setDao(SnapshotDefinitionDAO dao) {
-        super.setBaseDao(dao);
-    }
+	@Autowired
+	public void setDao(SnapshotDefinitionDAO dao) {
+		super.setBaseDao(dao);
+	}
 
-    @Override
-    public int save(SnapshotDefinition snapshotDefinition, String filename) throws LocalizedException {
+	@Override
+	public int save(SnapshotDefinition snapshotDefinition, String filename) throws LocalizedException {
 
-        List<String> parentIDList = new ArrayList<String>();
-        // The Value list is edited and added to the SnapshotDefinition object that should be attached.
-        for (SnapshotValue snapshotValue : snapshotDefinition.getSnapshotValues()) {
-            if (!snapshotValue.getParentTag().equals(Constants.VALUE_NULL) && snapshotValue.getRowIndex() != Constants.VALUE_CONTROL_ROWID
-                    && snapshotValue.getRowIndex() != Constants.VALUE_FILTER_ROWID && !parentIDList.contains(snapshotValue.getParentTag())) {
-                parentIDList.add(snapshotValue.getParentTag());
-            }
-        }
+		List<String> parentIDList = new ArrayList<String>();
+		// The Value list is edited and added to the SnapshotDefinition object that
+		// should be attached.
+		for (SnapshotValue snapshotValue : snapshotDefinition.getSnapshotValues()) {
+			if (!snapshotValue.getParentTag().equals(Constants.VALUE_NULL)
+					&& snapshotValue.getRowIndex() != Constants.VALUE_CONTROL_ROWID
+					&& snapshotValue.getRowIndex() != Constants.VALUE_FILTER_ROWID
+					&& !parentIDList.contains(snapshotValue.getParentTag())) {
+				parentIDList.add(snapshotValue.getParentTag());
+			}
+		}
 
-        Document doc = TedamDOMUtils.domParserStarter(filename);
-        if (doc == null)
-            throw new SnapshotFileNotFoundException(filename);
+		Document doc = TedamDOMUtils.domParserStarter(filename);
+		if (doc == null)
+			throw new SnapshotFileNotFoundException(filename);
 
-        // The Snapshot object is edited and added to the SnapshotDefinition object that should be attached.
-        Snapshot snapshot = new Snapshot();
-        snapshot.setXml(TedamDOMUtils.elementToString(doc.getDocumentElement()));
-        snapshot.setVersion(TedamXPathUtils.getVersionFromSnapshot(doc.getDocumentElement()));
-        snapshot.setFileName(filename);
-        snapshotDefinition.setSnapshot(snapshot);
+		// The Snapshot object is edited and added to the SnapshotDefinition object that
+		// should be attached.
+		Snapshot snapshot = new Snapshot();
+		snapshot.setXml(TedamDOMUtils.elementToString(doc.getDocumentElement()));
+		snapshot.setVersion(TedamXPathUtils.getVersionFromSnapshot(doc.getDocumentElement()));
+		snapshot.setFileName(filename);
+		snapshotDefinition.setSnapshot(snapshot);
 
-        // Possible Property objects are retrieved from the document and made ready for save operation.
-        // If the SplitterIndex -1 is a different value, the property table splitterIndex is processed in the appropriate format.
-        List<Property> propertyList = new ArrayList<Property>();
-        for (int i = 0; i < parentIDList.size(); i++) {
-            Property property = new Property();
-            String propertyValue = TedamXPathUtils.getPropertValueFromDocument(doc, parentIDList.get(i));
-            if (propertyValue != null && Integer.valueOf(propertyValue) != -1) {
-                property.setName(Constants.PROPERTY_SPLITTER);
+		// Possible Property objects are retrieved from the document and made ready for
+		// save operation.
+		// If the SplitterIndex -1 is a different value, the property table
+		// splitterIndex is processed in the appropriate format.
+		List<Property> propertyList = new ArrayList<Property>();
+		for (int i = 0; i < parentIDList.size(); i++) {
+			Property property = new Property();
+			String propertyValue = TedamXPathUtils.getPropertValueFromDocument(doc, parentIDList.get(i));
+			if (propertyValue != null && Integer.valueOf(propertyValue) != -1) {
+				property.setName(Constants.PROPERTY_SPLITTER);
 
-                property.setParameter(parentIDList.get(i));
-                property.setValue(propertyValue);
+				property.setParameter(parentIDList.get(i));
+				property.setValue(propertyValue);
 
-                propertyList.add(property);
-            }
-        }
+				propertyList.add(property);
+			}
+		}
 
-        int snapshotDefinitionId = save(snapshotDefinition).getId();
-        propertyList.forEach(property -> property.setParameter(snapshotDefinitionId + "," + property.getParameter()));
-        List<Property> propertyListInDB = propertyService.getPropertyListByCriteria(Constants.PROPERTY_SPLITTER, String.valueOf(snapshotDefinitionId), true);
-        List<Property> redefinePropertyList = propertyService.redefinePropertyList(propertyList, propertyListInDB);
-        for (Property property : redefinePropertyList) {
-            propertyService.save(property);
-        }
-        return snapshotDefinitionId;
+		int snapshotDefinitionId = save(snapshotDefinition).getId();
+		propertyList.forEach(property -> property.setParameter(snapshotDefinitionId + "," + property.getParameter()));
+		List<Property> propertyListInDB = propertyService.getPropertyListByCriteria(Constants.PROPERTY_SPLITTER,
+				String.valueOf(snapshotDefinitionId), true);
+		List<Property> redefinePropertyList = propertyService.redefinePropertyList(propertyList, propertyListInDB);
+		for (Property property : redefinePropertyList) {
+			propertyService.save(property);
+		}
+		return snapshotDefinitionId;
 
-    }
+	}
 
-    @Override
-    public void createLookupParameterContent(SnapshotDefinition snapshotDefinition, TestCase newTestCase) throws LocalizedException {
-        for (SnapshotValue snapshotValue : snapshotDefinition.getSnapshotValues()) {
-            if (StringUtils.isEmpty(snapshotValue.getLookUpParameter()) || snapshotValue.getLookUpParameter().equals(Constants.VALUE_NULL)) {
-                continue;
-            }
-            StringBuilder sbLookupParameter = new StringBuilder();
-            String[] lookupParameterArray = snapshotValue.getLookUpParameter().split("</(\\w+)>");
-            for (String lookupParameter : lookupParameterArray) {
-                String newLookupParameter = createNewLookUpParameter(newTestCase, lookupParameter);
-                sbLookupParameter.append(newLookupParameter);
-            }
-            snapshotValue.setLookUpParameter(sbLookupParameter.toString());
-        }
-    }
+	@Override
+	public void createLookupParameterContent(SnapshotDefinition snapshotDefinition, TestCase newTestCase)
+			throws LocalizedException {
+		for (SnapshotValue snapshotValue : snapshotDefinition.getSnapshotValues()) {
+			if (StringUtils.isEmpty(snapshotValue.getLookUpParameter())
+					|| snapshotValue.getLookUpParameter().equals(Constants.VALUE_NULL)) {
+				continue;
+			}
+			StringBuilder sbLookupParameter = new StringBuilder();
+			String[] lookupParameterArray = snapshotValue.getLookUpParameter().split("</(\\w+)>");
+			for (String lookupParameter : lookupParameterArray) {
+				String newLookupParameter = createNewLookUpParameter(newTestCase, lookupParameter);
+				sbLookupParameter.append(newLookupParameter);
+			}
+			snapshotValue.setLookUpParameter(sbLookupParameter.toString());
+		}
+	}
 
-    // the generator is created by determining which TestStep belongs to the incoming lookupParameter. The lookupParameter is fragmented and cloned to create the new one.
-    private String createNewLookUpParameter(TestCase newTestCase, String lookupParameter) throws LocalizedException {
-        for (TestStepType testStepType : TestStepType.getLookupParameterIsNotEmptyTestStepTypes()) {
-            if (lookupParameter.startsWith(testStepType.getBeginRegex())) {
-                Generator generator = GeneratorFactory.getGenerator(testStepType, beanFactory);
-                generator.degenerateLookUp(lookupParameter);
-                if (generator instanceof AbstractFillTestStepGenerator) {
-                    doFillTestStepOperations(newTestCase, generator);
-                }
-                return generator.generateLookUp();
-            }
-        }
-        return "";
-    }
+	// the generator is created by determining which TestStep belongs to the
+	// incoming lookupParameter. The lookupParameter is fragmented and cloned to
+	// create the new one.
+	private String createNewLookUpParameter(TestCase newTestCase, String lookupParameter) throws LocalizedException {
+		for (TestStepType testStepType : TestStepType.getLookupParameterIsNotEmptyTestStepTypes()) {
+			if (lookupParameter.startsWith(testStepType.getBeginRegex())) {
+				Generator generator = GeneratorFactory.getGenerator(testStepType, beanFactory);
+				generator.degenerateLookUp(lookupParameter);
+				if (generator instanceof AbstractFillTestStepGenerator) {
+					doFillTestStepOperations(newTestCase, generator);
+				}
+				return generator.generateLookUp();
+			}
+		}
+		return "";
+	}
 
-    private void doFillTestStepOperations(TestCase newTestCase, Generator generator) throws LocalizedException {
-        // The clone operation is performed and saved according to the snapshotDefinitionId contained in the lookup.
-        SnapshotDefinition tempSnapshotDefinition = ((AbstractFillTestStepGenerator) generator).getSnapshotDefinition();
-        SnapshotDefinition newSnapshotDefinition = tempSnapshotDefinition.cloneSnapshotDefinition();
-        newSnapshotDefinition = save(newSnapshotDefinition);
-        ((AbstractFillTestStepGenerator) generator).setSnapshotDefinition(newSnapshotDefinition);
-        findAndUpdateLookUp(String.valueOf(tempSnapshotDefinition.getId()), String.valueOf(newSnapshotDefinition.getId()), newTestCase.getLookUps());
-    }
+	private void doFillTestStepOperations(TestCase newTestCase, Generator generator) throws LocalizedException {
+		// The clone operation is performed and saved according to the
+		// snapshotDefinitionId contained in the lookup.
+		SnapshotDefinition tempSnapshotDefinition = ((AbstractFillTestStepGenerator) generator).getSnapshotDefinition();
+		SnapshotDefinition newSnapshotDefinition = tempSnapshotDefinition.cloneSnapshotDefinition();
+		newSnapshotDefinition = save(newSnapshotDefinition);
+		((AbstractFillTestStepGenerator) generator).setSnapshotDefinition(newSnapshotDefinition);
+		findAndUpdateLookUp(String.valueOf(tempSnapshotDefinition.getId()),
+				String.valueOf(newSnapshotDefinition.getId()), newTestCase.getLookUps());
+	}
 
-    /**
-     * this method findAndUpdateLookUp changes the old snapshotDefinitionId and the newly created snapshotDefinitionId to change the lookup parameters of testCase.<br>
-     *
-     * @param oldSnapshotDefinitionId
-     * @param newSnapshotDefinitionId
-     * @param lookUps                 <br>
-     * @author Canberk.Erkmen
-     */
-    private void findAndUpdateLookUp(String oldSnapshotDefinitionId, String newSnapshotDefinitionId, List<TestStep> lookUps) {
-        for (TestStep lookUp : lookUps) {
-            lookUp.setParameter(lookUp.getParameter().replaceAll(oldSnapshotDefinitionId, newSnapshotDefinitionId));
-        }
-    }
+	/**
+	 * this method findAndUpdateLookUp changes the old snapshotDefinitionId and the
+	 * newly created snapshotDefinitionId to change the lookup parameters of
+	 * testCase.<br>
+	 *
+	 * @param oldSnapshotDefinitionId
+	 * @param newSnapshotDefinitionId
+	 * @param lookUps                 <br>
+	 * @author Canberk.Erkmen
+	 */
+	private void findAndUpdateLookUp(String oldSnapshotDefinitionId, String newSnapshotDefinitionId,
+			List<TestStep> lookUps) {
+		for (TestStep lookUp : lookUps) {
+			lookUp.setParameter(lookUp.getParameter().replaceAll(oldSnapshotDefinitionId, newSnapshotDefinitionId));
+		}
+	}
 }
--- src/main/java/com/lbs/tedam/websocket/client/WebSocketClient.java
+++ src/main/java/com/lbs/tedam/websocket/client/WebSocketClient.java
@@ -17,11 +17,6 @@
 
 package com.lbs.tedam.websocket.client;
 
-import com.lbs.tedam.util.Constants;
-import com.lbs.tedam.util.TedamProcessUtils;
-import com.lbs.tedam.websocket.WebSocketException;
-
-import javax.websocket.*;
 import java.io.IOException;
 import java.io.Serializable;
 import java.net.URI;
@@ -29,171 +24,189 @@ import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.websocket.ClientEndpoint;
+import javax.websocket.CloseReason;
+import javax.websocket.ContainerProvider;
+import javax.websocket.DeploymentException;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.WebSocketContainer;
+
+import com.lbs.tedam.util.Constants;
+import com.lbs.tedam.util.TedamProcessUtils;
+import com.lbs.tedam.websocket.WebSocketException;
+
 /**
  * Base class for web socket clients.
  */
 @ClientEndpoint
 public class WebSocketClient implements Serializable {
 
-    /**
-     * Default serial version UID.
-     */
-    private static final long serialVersionUID = 1L;
+	/**
+	 * Default serial version UID.
+	 */
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * Web socket client session.
-     */
-    private Session session = null;
+	/**
+	 * Web socket client session.
+	 */
+	private transient Session session = null;
 
-    /**
-     * Web socket server URI.
-     */
-    private String serverURI = null;
+	/**
+	 * Web socket server URI.
+	 */
+	private String serverURI = null;
 
-    /**
-     * Listener list for session operation.
-     */
-    private List<WebSocketClientListener> listenerList = new ArrayList<>();
+	/**
+	 * Listener list for session operation.
+	 */
+	private List<WebSocketClientListener> listenerList = new ArrayList<>();
 
-    /**
-     * One parameter constructor.
-     *
-     * @param serverURI Web socket server URI.
-     */
-    public WebSocketClient(String serverURI) {
-        this.serverURI = serverURI;
-    }
+	/**
+	 * One parameter constructor.
+	 *
+	 * @param serverURI Web socket server URI.
+	 */
+	public WebSocketClient(String serverURI) {
+		this.serverURI = serverURI;
+	}
 
-    /**
-     * This method will be called when a web socket session is opened.
-     *
-     * @param session Session info on web socket session.
-     */
-    @OnOpen
-    public void onOpen(Session session) {
-        setSession(session);
-        for (WebSocketClientListener listener : listenerList)
-            listener.onOpen(session);
-    }
+	/**
+	 * This method will be called when a web socket session is opened.
+	 *
+	 * @param session Session info on web socket session.
+	 */
+	@OnOpen
+	public void onOpen(Session session) {
+		setSession(session);
+		for (WebSocketClientListener listener : listenerList)
+			listener.onOpen(session);
+	}
 
-    /**
-     * This method will be called when a web socket session is closed.
-     *
-     * @param session     Session info on web socket session.
-     * @param closeReason Close reason for why session is closed.
-     */
-    @OnClose
-    public void onClose(Session session, CloseReason closeReason) {
-        for (WebSocketClientListener listener : listenerList)
-            listener.onClose(session, closeReason);
-    }
+	/**
+	 * This method will be called when a web socket session is closed.
+	 *
+	 * @param session     Session info on web socket session.
+	 * @param closeReason Close reason for why session is closed.
+	 */
+	@OnClose
+	public void onClose(Session session, CloseReason closeReason) {
+		for (WebSocketClientListener listener : listenerList)
+			listener.onClose(session, closeReason);
+	}
 
-    /**
-     * This method will be called when a web socket session received a message.
-     *
-     * @param message String message that is received on session.
-     */
-    @OnMessage
-    public void onMessage(String message) {
-        for (WebSocketClientListener listener : listenerList)
-            listener.onMessage(message);
-    }
+	/**
+	 * This method will be called when a web socket session received a message.
+	 *
+	 * @param message String message that is received on session.
+	 */
+	@OnMessage
+	public void onMessage(String message) {
+		for (WebSocketClientListener listener : listenerList)
+			listener.onMessage(message);
+	}
 
-    /**
-     * This method will be called when a web socket session received an error.
-     *
-     * @param error Error instance.
-     */
-    @OnError
-    public void onError(Throwable error) {
-        for (WebSocketClientListener listener : listenerList)
-            listener.onError(error);
-    }
+	/**
+	 * This method will be called when a web socket session received an error.
+	 *
+	 * @param error Error instance.
+	 */
+	@OnError
+	public void onError(Throwable error) {
+		for (WebSocketClientListener listener : listenerList)
+			listener.onError(error);
+	}
 
-    /**
-     * Connects to web socket server with given server URI. If there is a valid session, it will be closed. This method is one try method. If wants to connect until connection is
-     * successful, use reconnect().
-     *
-     * @throws WebSocketException Throws when an exception occurs during connection.
-     */
-    public void connect() throws WebSocketException {
-        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
-        try {
-            container.connectToServer(this, new URI(serverURI));
-        } catch (DeploymentException | IOException | URISyntaxException e) {
-            throw new WebSocketException("An exception occured during connecting to web socket server.", e);
-        }
-    }
+	/**
+	 * Connects to web socket server with given server URI. If there is a valid
+	 * session, it will be closed. This method is one try method. If wants to
+	 * connect until connection is successful, use reconnect().
+	 *
+	 * @throws WebSocketException Throws when an exception occurs during connection.
+	 */
+	public void connect() throws WebSocketException {
+		WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+		try {
+			container.connectToServer(this, new URI(serverURI));
+		} catch (DeploymentException | IOException | URISyntaxException e) {
+			throw new WebSocketException("An exception occured during connecting to web socket server.", e);
+		}
+	}
 
-    /**
-     * Connects to web socket server with given server URI. If there is a valid session, it will be closed. If conenction is not successful, will try again until connection is
-     * successful.
-     *
-     * @throws WebSocketException
-     */
-    public synchronized void reconnect() {
-        while (!isSessionValid()) {
-            try {
-                connect();
-            } catch (WebSocketException e) {
-                TedamProcessUtils.sleepThread(Constants.THREAD_SLEEP_TIME);
-            }
-        }
-    }
+	/**
+	 * Connects to web socket server with given server URI. If there is a valid
+	 * session, it will be closed. If conenction is not successful, will try again
+	 * until connection is successful.
+	 *
+	 * @throws WebSocketException
+	 */
+	public synchronized void reconnect() {
+		while (!isSessionValid()) {
+			try {
+				connect();
+			} catch (WebSocketException e) {
+				TedamProcessUtils.sleepThread(Constants.THREAD_SLEEP_TIME);
+			}
+		}
+	}
 
-    /**
-     * Disconnects from web socket server.
-     *
-     * @throws WebSocketException Throws when an exception occurs during disconnection.
-     */
-    public void disconnect() throws WebSocketException {
-        if (isSessionValid())
-            try {
-                getSession().close();
-            } catch (IOException e) {
-                throw new WebSocketException("An exception occured during closing connection.", e);
-            }
-    }
+	/**
+	 * Disconnects from web socket server.
+	 *
+	 * @throws WebSocketException Throws when an exception occurs during
+	 *                            disconnection.
+	 */
+	public void disconnect() throws WebSocketException {
+		if (isSessionValid())
+			try {
+				getSession().close();
+			} catch (IOException e) {
+				throw new WebSocketException("An exception occured during closing connection.", e);
+			}
+	}
 
-    /**
-     * Checks if session instance is valid or not.
-     *
-     * @return If session is not null and open then true, else false.
-     */
-    public boolean isSessionValid() {
-        return getSession() != null && getSession().isOpen();
-    }
+	/**
+	 * Checks if session instance is valid or not.
+	 *
+	 * @return If session is not null and open then true, else false.
+	 */
+	public boolean isSessionValid() {
+		return getSession() != null && getSession().isOpen();
+	}
 
-    /**
-     * @return the session
-     */
-    public Session getSession() {
-        return session;
-    }
+	/**
+	 * @return the session
+	 */
+	public Session getSession() {
+		return session;
+	}
 
-    /**
-     * @param session the session to set
-     */
-    private void setSession(Session session) {
-        this.session = session;
-    }
+	/**
+	 * @param session the session to set
+	 */
+	private void setSession(Session session) {
+		this.session = session;
+	}
 
-    /**
-     * Adds WebSocketClientListener to list.
-     *
-     * @param listener Instance to add to list.
-     */
-    public void addWebSocketClientListener(WebSocketClientListener listener) {
-        listenerList.add(listener);
-    }
+	/**
+	 * Adds WebSocketClientListener to list.
+	 *
+	 * @param listener Instance to add to list.
+	 */
+	public void addWebSocketClientListener(WebSocketClientListener listener) {
+		listenerList.add(listener);
+	}
 
-    /**
-     * Gets web socket server URI given on constructor.
-     *
-     * @return Complete path of web socket server URI.
-     */
-    public String getServerURI() {
-        return serverURI;
-    }
+	/**
+	 * Gets web socket server URI given on constructor.
+	 *
+	 * @return Complete path of web socket server URI.
+	 */
+	public String getServerURI() {
+		return serverURI;
+	}
 
 }
