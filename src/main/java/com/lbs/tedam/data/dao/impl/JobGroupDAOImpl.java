a48ab939c7cb3fc4f4723bc4ccc2076f09d9f89c
Can Berk İzgi <canberkizgi22@gmail.com>
2018-12-29 09:57:11 +0300

    Implement jobgroup(#9)

---
+++ src/main/java/com/lbs/tedam/data/dao/impl/JobGroupDAOImpl.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2014-2019 Logo Business Solutions
+ * (a.k.a. LOGO YAZILIM SAN. VE TIC. A.S)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.lbs.tedam.data.dao.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.lbs.tedam.data.dao.JobGroupDAO;
+import com.lbs.tedam.data.repository.JobGroupRepository;
+import com.lbs.tedam.exception.localized.GeneralLocalizedException;
+import com.lbs.tedam.exception.localized.LocalizedException;
+import com.lbs.tedam.model.JobGroup;
+import com.lbs.tedam.model.Project;
+import com.lbs.tedam.util.EnumsV2.TedamBoolean;
+
+@Component
+public class JobGroupDAOImpl extends BaseDAOImpl<JobGroup, Integer> implements JobGroupDAO {
+
+	/**
+	 * long serialVersionUID
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private transient JobGroupRepository repository;
+
+	@Autowired
+	public void setRepository(JobGroupRepository repository) {
+		this.repository = repository;
+		super.setRepository(repository);
+	}
+
+	@Override
+	public List<JobGroup> getJobGroupList(Project project) throws LocalizedException {
+		try {
+			List<JobGroup> jobGroupList = repository.findByProjectAndDeleted(project, TedamBoolean.FALSE.getBooleanValue());
+			return jobGroupList;
+		} catch (Exception e) {
+			throw new GeneralLocalizedException(e);
+		}
+	}
+
+}
---
+++ src/main/java/com/lbs/tedam/model/JobGroup.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2014-2019 Logo Business Solutions
+ * (a.k.a. LOGO YAZILIM SAN. VE TIC. A.S)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.lbs.tedam.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.Where;
+
+@Entity
+@Where(clause = "IS_DELETED=0")
+@Table(name = "JOB_GROUP")
+public class JobGroup extends AbstractBaseEntity {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4378420222319078983L;
+
+	@Column(name = "NAME", unique = true)
+	@Size(min = 1, max = 255)
+	private String name;
+
+	@Column(name = "DESCRIPTION")
+	private String description;
+
+	@ManyToOne
+	@JoinColumn(name = "PROJECT_ID")
+	@Where(clause = "IS_DELETED=0")
+	private Project project;
+
+	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Job> jobs = new ArrayList<>();
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Project getProject() {
+		return project;
+	}
+
+	public void setProject(Project project) {
+		this.project = project;
+	}
+
+	public List<Job> getJobs() {
+		return jobs;
+	}
+
+	public void setJobs(List<Job> jobs) {
+		this.jobs = jobs;
+	}
+
+}
